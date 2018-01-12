d820b883a6c213e83a99c2d17d0ee7c7c678d410
mbanaouas <mbana@free.fr>
2018-01-12 12:24:01 +0100

    hello

--- alpha/settings.py
+++ alpha/settings.py
@@ -165,7 +165,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
-
-STATIC_ROOT = '/home/cerib33/alpha/static'
+STATIC_ROOT = config.get('static', 'STATIC_ROOT')
+STATICFILES_DIRS = config.get('static', 'STATICFILES_DIRS').split(',')
